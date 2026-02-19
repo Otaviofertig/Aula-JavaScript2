e4d1a69fde0b9bc1e6443ff4d5fbe302900b9ee0
Otaviofertig <otavio.lesss@gmail.com>
2026-02-19 16:55:04 -0300

    commit

--- scripit.js
+++ scripit.js
@@ -3,6 +3,14 @@ const carros = ["Uno", "Escort", "Gol"]
 
 const conteudo = document.getElementById("conteudo");
 
+function criaSecao(titulo,dadosf) {
+    let secao = document.createElement("div");
+secao.innerHTML = "<h2>Loop While</h2>"+dados;
+conteudo.appendChild(secao);
+}
+
+
+
 let dados ="";
 let i = 0;
 while(i < 4) 
@@ -11,6 +19,14 @@ while(i < 4)
     i++;
 }
 
-const secao = document.createElement("div");
-secao.innerHTML = "<h2>Loop While</h2>"+dados;
-conteudo.appendChild(secao);
+criaSecao("Loop While", dados);
+
+criaSecao("Loop While 2", dados2)
+
+let dados2 ="";
+let i2 = 0;
+while(i2 < carros.length) 
+{
+    dados2 += "<p>" + carros[i2] +  "<//p>";
+    i2++;
+}
